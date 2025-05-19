0038c16bc57a3c3c01fbecbf5beed4654a0d2b62
Luis Angel <luis.muniz@encora.com>
2025-05-19 16:22:44 -0600

    Loggs Implementation

--- products/src/main/java/com/inventory/products/controllers/CategoryController.java
+++ products/src/main/java/com/inventory/products/controllers/CategoryController.java
@@ -2,6 +2,7 @@ package com.inventory.products.controllers;
 
 import com.inventory.products.model.Category;
 import com.inventory.products.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
@@ -21,19 +23,25 @@ public class CategoryController {
 
     @PostMapping
     public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        log.info("Received request to create category: {}", category);
         Category createdCategory = categoryService.createCategory(category);
+        log.info("Category created successfully: {}", createdCategory);
         return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<Category>> getAllCategories() {
+        log.info("Received request to get all categories");
         List<Category> categories = categoryService.getAllCategories();
+        log.info("Returning {} categories", categories.size());
         return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
     @GetMapping("/{name}")
     public ResponseEntity<Category> getCategoryByName(@PathVariable String name) {
+        log.info("Received request to get category by name: {}", name);
         Category category = categoryService.getCategoryByName(name);
+        log.info("Returning category: {}", category);
         return new ResponseEntity<>(category, HttpStatus.OK);
     }
 }
--- products/src/main/java/com/inventory/products/controllers/InventoryExceptionHandler.java
+++ products/src/main/java/com/inventory/products/controllers/InventoryExceptionHandler.java
@@ -3,28 +3,33 @@ package com.inventory.products.controllers;
 import com.inventory.products.dto.ErrorResponse;
 import com.inventory.products.exception.EntityAlreadyExistsException;
 import com.inventory.products.exception.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 
+@Slf4j
 @ControllerAdvice
 public class InventoryExceptionHandler {
     @ExceptionHandler(EntityAlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleCategoryAlreadyExistsException(EntityAlreadyExistsException ex) {
+        log.error("Category already exists", ex);
         ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleCategoryNotFoundException(EntityNotFoundException ex) {
+        log.error("Category not found", ex);
         ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        log.error("Invalid argument", ex);
         ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
